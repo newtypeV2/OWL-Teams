75b457d470c5986b0e61d21ac779626c1cbfcb96
Gian Libunao <gian.Libunao@reisystems.com>
2019-12-10 10:31:20 -0500

    Added tests.

---
+++ OWLUnitTest/EssentialTrainingTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OWL_Teams.Models;
+
+namespace OWLUnitTest
+{
+    [TestClass]
+    public class EssentialTrainingTests
+    {
+        [TestMethod]
+        public void ModelTest()
+        {
+            var team1 = new Team
+            {
+                Name = "Test Team name",
+                City = "Test City name",
+                Id = 999
+            };
+            Assert.AreEqual("Test Team name", team1.Name, "I expect Team name to be 'Test Team name'");
+            Assert.AreEqual("Test City name", team1.City, "I expect Team city to be 'Test City name'");
+            Assert.AreEqual(999, team1.Id, "I expect Id to be '999'");
+        }
+    }
+}
---
+++ OWLUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OWLUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OWLUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fdbd957d-25b7-4d06-b5b5-877c198e91c6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
